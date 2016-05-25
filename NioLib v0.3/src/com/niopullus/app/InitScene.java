2ccdba269b6379e1ee2dceaafd841337b2b7f78a
Niopullus <owenvnek@gmail.com>
2016-05-24 20:09:28 -0400

    Updated Code

--- NioLib v0.3/src/com/niopullus/app/InitScene.java
+++ NioLib v0.3/src/com/niopullus/app/InitScene.java
@@ -1,14 +1,6 @@
 package com.niopullus.app;
 
-import com.niopullus.NioLib.Main;
-import com.niopullus.NioLib.scene.Background;
-import com.niopullus.NioLib.scene.DynamicImageBackground;
-import com.niopullus.NioLib.scene.ImageBackground;
 import com.niopullus.NioLib.scene.guiscene.*;
-import com.niopullus.NioLib.scene.guiscene.Button;
-import com.niopullus.NioLib.scene.guiscene.Label;
-import com.niopullus.NioLib.scene.mapeditorscene.MapEditorScene;
-import com.niopullus.NioLib.scene.mapeditorscene.WorldPickScene;
 
 import java.awt.*;
 
